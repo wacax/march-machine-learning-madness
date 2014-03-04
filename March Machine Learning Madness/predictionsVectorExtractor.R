8a7a98fa0f8676e2676baf234285439946e1dfce
wacax <wacax@hotmail.com>
2014-03-03 19:02:26 -0500

    commit 3rd

--- March Machine Learning Madness/predictionsVectorExtractor.R
+++ March Machine Learning Madness/predictionsVectorExtractor.R
@@ -1,17 +1,27 @@
 predictionsVectorExtractor <- function(string, seasonResults, tourneyResults, tourneySeeds){
   
   arf <- strsplit(string, '_')  
-  season <- sapply(arf, anonFun <- function(anonList){anonList[1]
+  seasonVector <- sapply(arf, anonFun <- function(anonList){anonList[1]
   })
   home <- sapply(arf, anonFun <- function(anonList){anonList[2]
   })
   visitor <- sapply(arf, anonFun <- function(anonList){anonList[3]
   })
   
-  homeSeed <- sapply(home, anonFun <- function(homeItem, season){
-    tourneySeeds$seed[tourneySeeds$team == homeItem & tourneySeeds$season == season]    
-  })
-  visitorSeed <- sapply(visitor, anonFun <- function(awayItem, season){
-    tourneySeeds$seed[tourneySeeds$team == awayItem & tourneySeeds$season == season]    
-  })
+  #home <- cbind(seasonVector, home)
+  homeSeeds <- rep(0, length(home))
+  visitorSeeds <- rep(0, length(home))
+  
+    for(i in 1:length(home)){
+    homeSeeds[i] <- tourneySeeds$seed[tourneySeeds$team == home[i] & tourneySeeds$season == seasonVector[i]]    
+  }
+  
+  for(i in 1:length(visitor)){
+    visitorSeeds[i] <- tourneySeeds$seed[tourneySeeds$team == visitor[i] & tourneySeeds$season == seasonVector[i]]    
+  }
+  
+  homeSeeds <- as.numeric(gsub('[A-Za-z]', '', homeSeeds))
+  visitorSeeds <- as.numeric(gsub('[A-Za-z]', '', visitorSeeds))
+  
+  
 }
