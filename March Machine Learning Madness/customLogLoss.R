79b9255164e98c5d305e41100bdaf1087dd8c562
wacax <wacax@hotmail.com>
2014-03-05 22:56:21 -0500

    first prediction 0.60 pt.2

--- March Machine Learning Madness/customLogLoss.R
+++ March Machine Learning Madness/customLogLoss.R
@@ -3,7 +3,6 @@ customLogLoss <- function(actual, predicted){
   logPred <- log(predicted)
   logPred[logPred == -Inf] <- -20
   
-  return(-(sum(actual * logPred + (1 - actual) * log(1 - predicted)) / length(actual)))
-  
+  return(-(sum(actual * logPred + (1 - actual) * log(1 - predicted)) / length(actual)))  
 
 }
