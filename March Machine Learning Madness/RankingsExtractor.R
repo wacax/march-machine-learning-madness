f8a337f9dc2f08c1bbe167e64af282a59a208925
wacax <wacax@hotmail.com>
2014-03-11 23:21:14 -0500

    core33 + nonCore ; w/NAs

--- March Machine Learning Madness/RankingsExtractor.R
+++ March Machine Learning Madness/RankingsExtractor.R
@@ -77,15 +77,62 @@ RankingsExtractor <- function(teams1, teams2, seasonVector, dateVec = NULL){
   }
   coreRankings2 <- as.data.frame(coreRankings2)
   names(coreRankings) <- c("sagarinRankings","ordinalRank","AP2","DUN2","MOR2","POM2","RPI2","SAG2","SE2","USA2","WLK2","BOB2","RTH2","WOL2","COL2","DOL2","CNG2","DES2","DC2","WIL2","DOK2","PIG2","MB2","RTR2","CPR2","REW2","STH2","SPW2","PGH2","CPA2","RTB2","BPI2","NOL2","DCI","LMC2")
-######################################
-#Under construction
-  #non-Core  
-  namesOR <- unique(nonCore$sys_name)
-  originalNamesOR <- sort(unique(nonCore$sys_name))
-  lenOR <- length(unique(nonCore$sys_name))
-  seasonIdx <- nonCore$season == seasonInt
   
-######################################
+  #non-Core  
+  nonCorerankingExtract <- function(teamInt, seasonInt, dateInt){
+    namesOR <- unique(nonCore$sys_name)
+    originalNamesOR <- sort(unique(nonCore$sys_name))
+    lenOR <- length(unique(nonCore$sys_name))
+    seasonIdx <- nonCore$season == seasonInt
+    
+    if (sum(seasonIdx) == 0){
+      ordinalRank <- rep(NA, length(namesOR))
+      names(ordinalRank) <- namesOR
+    }else{
+      nonCoreCut <- nonCore[seasonIdx, ]
+      teamIdx <- nonCoreCut$team == teamInt
+      nonCoreOrdinalRank <- nonCoreCut[teamIdx, ]
+      rankIdxs <- abs(nonCoreOrdinalRank$rating_day_num - dateInt) %in% min(abs(nonCoreOrdinalRank$rating_day_num - dateInt)) 
+      nonCoreOrdinalRank <- nonCoreOrdinalRank[rankIdxs, ]
+      namesOR <- nonCoreOrdinalRank$sys_name
+      nonCoreOrdinalRank <- nonCoreOrdinalRank$orank 
+      if(length(nonCoreOrdinalRank) < lenOR){
+        names(nonCoreOrdinalRank) <- namesOR
+        ordinalRankNew <- rep(NA, lenOR)
+        for(ii in 1:length(nonCoreOrdinalRank)){
+          ordinalRankNew[originalNamesOR %in% names(nonCoreOrdinalRank[ii])] <- nonCoreOrdinalRank[ii]
+        }
+        nonCoreOrdinalRank <- ordinalRankNew
+        names(nonCoreOrdinalRank) <- originalNamesOR
+      }else{
+        names(nonCoreOrdinalRank) <- namesOR
+      }
+    }
+    if(length(nonCoreOrdinalRank) > lenOR){
+      return(rep(NA, lenOR))
+    }else{
+      return(nonCoreOrdinalRank)
+    }
+  }
+    
+  nonCoreRankings <- matrix(rep(0, length(seasonVector) * (length(unique(nonCore$sys_name)))), 
+                         nrow = length(seasonVector), ncol = (length(unique(nonCore$sys_name))))
+  for(i in 1:length(seasonVector)){
+    nonCoreRankings[i, ] <- nonCorerankingExtract(teams1[i], seasonVector[i], dateVec[i])    
+  }
+  nonCoreRankings <- as.data.frame(nonCoreRankings)
+  names(nonCoreRankings) <- sort(unique(nonCore$sys_name))
+  
+  nonCoreRankings2 <- matrix(rep(0, length(seasonVector) * (length(unique(nonCore$sys_name)))), 
+                            nrow = length(seasonVector), ncol = (length(unique(nonCore$sys_name))))
+  for(i in 1:length(seasonVector)){
+    nonCoreRankings2[i, ] <- nonCorerankingExtract(teams1[i], seasonVector[i], dateVec[i])    
+  }
+  nonCoreRankings2 <- as.data.frame(nonCoreRankings2)
+  names(nonCoreRankings2) <- sapply(sort(unique(nonCore$sys_name)), anonFun <-function(string){
+    return(paste0(string, 2))
+  })    
+
   #Seeds extractor
   extractSeeds <- function(teamVector, seasonVector){
     Seeds <- rep(0, nrow(tourneyResults))
@@ -103,5 +150,5 @@ RankingsExtractor <- function(teams1, teams2, seasonVector, dateVec = NULL){
   
   
   
-  return(cbind(seedsTeam1, seedsTeam2, seedBenchmark, coreRankings, coreRankings2))
+  return(cbind(seedsTeam1, seedsTeam2, seedBenchmark, coreRankings, coreRankings2, nonCoreRankings1, nonCoreRankings2))
 }
